5d708e8dac3c7efcacb34bf698fe4ce1bd212d6b
James P Bondeson <jpbond58@gmail.com>
2020-08-23 01:54:49 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -11,7 +11,7 @@
 # end
 def run_guessing_game
 	roll = 1+rand(6)
-  user_input = gets.chomp.to_s
+  user_input = gets.chomp#.to_s
   if user_input == "exit"
     puts "Goodbye!"
   elsif  user_input = roll.to_s
