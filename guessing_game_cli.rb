184df30373abe53f9c19082cb6dbe7024a5b2b2e
James P Bondeson <jpbond58@gmail.com>
2020-08-23 01:23:11 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -5,5 +5,7 @@ def run_guessing_game
     puts "Goodbye!"
   elsif user_input != roll
     puts "Sorry! The computer guessed #{roll}."
+  else user_input == roll
+    puts "You guessed the correct number!"
   end
 end
